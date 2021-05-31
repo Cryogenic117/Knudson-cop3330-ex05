f3aecb38a9b417965bdf104ddb6a903862e7c93f
John Doe <bknudson117@gmail.com>
2021-05-31 13:21:43 -0400

    commit 1

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,26 @@
+package oop.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Scanner input = new Scanner(System.in);
+        String num1, num2;
+        Integer num1Int, num2Int;
+
+        System.out.println("What is the first number? ");
+        num1 = input.nextLine();
+        num1Int = Integer.parseInt(num1);
+
+        System.out.println("What is the second number? ");
+        num2 = input.nextLine();
+        num2Int = Integer.parseInt(num2);
+
+        System.out.printf("%d + %d = %d \n%d - %d = %d\n%d * %d = %d\n%d / %d = %d", num1Int, num2Int, num1Int+num2Int, num1Int, num2Int, num1Int-num2Int, num1Int, num2Int, num1Int*num2Int, num1Int, num2Int, num1Int/num2Int);
+
+
+
+
+
+    }
+}
